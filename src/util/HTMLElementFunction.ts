3307b2a1ea64a12bc0240deeb56214e12cd539cd
TomRaaff <tom.raaff@jcore.com>
2021-06-14 21:26:53 +0200

    added ideas

--- src/util/HTMLElementFunction.ts
+++ src/util/HTMLElementFunction.ts
@@ -1,5 +1,7 @@
 import { GlobalAttributes } from './GlobalAttributes';
 
+// todo: try: ...spread all input params
+// todo: try: currying: div(attributes?)(innerHTML?)
 export interface HTMLElementFunction {
 	(attributes: GlobalAttributes, innerContent?: Array<string|HTMLElement>): HTMLElement
 }
